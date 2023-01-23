905a8eba1ef967b3a266704ac08fc254b26c9da9
ruslanyar <styxrus@yandex.ru>
2023-01-23 12:54:30 +0300

    add .env examples files

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
       repo: 'git@github.com:ruslanyar/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': 'cd backend && npm i && npm run build && pm2 reload ecosystem.config.js',
+      'post-deploy': 'cd ../source/backend && npm i && npm run build && pm2 reload ecosystem.config.js && pm2 save',
     },
   },
 };
