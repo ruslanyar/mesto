14486ded32943261e2796c1f6f610de4ee396c33
ruslanyar <styxrus@yandex.ru>
2023-02-19 21:51:45 +0300

    edit ecosystem for backend

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'pre-deploy-local': 'bash ./scripts/deployEnv.sh',
       'post-deploy': 'cd ../source/backend && npm i && npm run build && pm2 reload ecosystem.config.js && pm2 save',
     },
   },
