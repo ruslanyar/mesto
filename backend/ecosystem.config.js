a52b3833b5f3010ad524bd20d2f8e38758d187e8
ruslanyar <styxrus@yandex.ru>
2023-04-21 22:25:32 +0300

    minor fixes

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,11 +1,7 @@
 require('dotenv').config({ path: './.env.deploy' });
 
 const {
-  DEPLOY_USER,
-  DEPLOY_HOST,
-  DEPLOY_REF,
-  DEPLOY_REPO,
-  DEPLOY_PATH,
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_REPO, DEPLOY_PATH,
 } = process.env;
 
 module.exports = {
@@ -23,8 +19,9 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': 'bash ./scripts/deployEnv.sh',
-      'post-deploy': 'cd ../source/backend && npm i && npm run build && pm2 reload ecosystem.config.js && pm2 save',
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'post-deploy':
+        'cd ../source/backend && npm i && npm run build && pm2 reload ecosystem.config.js && pm2 save',
     },
   },
 };
