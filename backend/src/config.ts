ee9a33f1759ba6be2644d43ec35b37041dcfbb97
ruslanyar <styxrus@yandex.ru>
2023-04-24 20:47:07 +0300

    fix cors

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -7,4 +7,4 @@ console.log(process.env);
 export const { JWT_SECRET = 'JWT_SECRET' } = process.env;
 export const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 export const { PORT = 3000 } = process.env;
-export const { ALLOWED_URL = '*' } = process.env;
+export const ALLOWED_URL = process.env.ALLOWED_URL?.split(',') || '*';
